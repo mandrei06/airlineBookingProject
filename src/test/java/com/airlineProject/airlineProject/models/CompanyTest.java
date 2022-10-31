5cabf547cda799ffa4a13e6f8fad0423578432e3
Sean-Hallinan <seanhallinan123@gmail.com>
2022-10-31 15:25:19 +0100

    Company TDD and Implement

---
+++ src/test/java/com/airlineProject/airlineProject/models/CompanyTest.java
@@ -0,0 +1,46 @@
+package com.airlineProject.airlineProject.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyTest {
+
+    Company company;
+
+    @BeforeEach
+    void initializer() {
+        company = new Company();
+        company.setId(5);
+        company.setName("COMPANY!");
+    }
+
+    @Test
+    void getId() {
+        Assertions.assertEquals(5, company.getId());
+
+    }
+
+    @Test
+    void setId() {
+        company.setId(6);
+        Assertions.assertEquals(6, company.getId());
+    }
+
+    @Test
+    void getName() {
+        Assertions.assertEquals("COMPANY!", company.getName());
+    }
+
+    @Test
+    void setName() {
+        company.setName("Test");
+        Assertions.assertEquals("Test", company.getName());
+    }
+
+    @Test
+    void testToString() {
+    }
+}
