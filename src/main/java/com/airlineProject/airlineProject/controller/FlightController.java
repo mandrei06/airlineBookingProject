bbb71b57f8dd685580bc7f5a895106548512cfce
Petros Grivas <petros.grivas@solera.com>
2022-11-02 10:17:08 +0100

    flight microservice

--- src/main/java/com/airlineProject/airlineProject/controller/FlightController.java
+++ src/main/java/com/airlineProject/airlineProject/controller/FlightController.java
@@ -1,9 +1,23 @@
 package com.airlineProject.airlineProject.controller;
 
+import com.airlineProject.airlineProject.model.Flight;
+import com.airlineProject.airlineProject.service.FlightService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/flights")
 public class FlightController {
+    FlightService flightService;
+
+    @PostMapping("/")
+    public Flight saveFlight(@RequestBody Flight flight){
+        return flightService.saveFlight(flight);
+    }
+    @GetMapping("/{id}")
+    public Flight findFlightById(@PathVariable("id") Integer flightId){
+        return flightService.findFlightById(flightId);
+    }
 
 
 }
