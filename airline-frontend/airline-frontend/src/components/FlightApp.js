982919d36f2f3007636063c3773d57409cdc35cf
Andy <54832870+mandrei06@users.noreply.github.com>
2022-11-02 11:37:59 +0100

    FrontEnd

---
+++ airline-frontend/airline-frontend/src/components/FlightApp.js
@@ -0,0 +1,126 @@
+import React from "react";
+import './FlightApp.css'
+
+const FlightApp = () => {
+    return (
+    <React.Fragment>
+        <section>
+            <form style={{border: '5px solid rgba(0, 0, 0, 0.05)', textAlign: "center"}}>
+                <header className="headDiv" >
+                    {/* Header Section */}
+                    <div className="h-24 flex justify-center items-center shadow" style={{height: 24}}>
+                        <h1 className="uppercase font-bold text-4xl text-center" style={{backgroundColor: "gray"}}>Flight booking app</h1>
+                    </div>
+                </header>
+                <br></br> 
+                {/* body Section */}
+                <div style={{backgroundColor: "lightgray", textAlign: "center"}}>
+                    <div>
+                        <div>
+                            <div className="Div1">
+                                <div className="flex items-center space-x-2" style={{felx: 1}} >
+                                    <input
+                                    type="radio"
+                                    className="w-6 h-6"
+                                    ></input>
+                                    <p className="text-xl font-bold">Round Trip</p>
+                                    <input
+                                    type="radio"
+                                    className="w-6 h-6"
+                                    ></input>
+                                    <p className="text-xl font-bold"  style={{felx: 0}}>One Way</p>
+                                    <br></br>
+                                </div>
+                            </div>
+                            <div>Error</div>
+                        </div>
+                        {/* Departure */}
+                        <div>
+                            <div>
+                                <div>
+                                    <b >Flying From?</b>
+                                    <select>
+                                        <option>--Select Airtport--</option>
+                                    </select>
+                                    <br></br>
+                                    <br></br>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Arrival */}
+                        <div>
+                            <div>
+                                <div>
+                                    <b >Flying To?</b>
+                                    <select>
+                                        <option>--Select Airtport--</option>
+                                    </select>
+                                    <br></br>
+                                </div>
+                                <div>Error</div>
+                            </div>
+                        ---------------------------------------
+                        </div>
+                        {/**date selection */}
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <b>Departure Date</b>
+                                        <input type='date'c/>
+                                    </div>
+                                    <div>Error</div>
+                                </div>
+                            </div>
+                        </div>
+                        -----------------------------------------
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <b>Return Date</b>
+                                        <input type='date'c/>
+                                    </div>
+                                    <div>Error</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <b>Number of Passengers Older than 9</b>
+                                        <input type="number" max={10} min={0} value={0}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <b>Number of Passengers between 9 and 2</b>
+                                        <input type="number" max={10} min={0} value={0}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <b>Number of Passengers younger than 10</b>
+                                        <input type="number" max={10} min={0} value={0}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </React.Fragment>
+    );
+};
+
+export default FlightApp;
---
+++ airline-frontend/airline-frontend/src/components/PassengerInfo.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const PassengerInfo = () => {
+    return (
+<h1>PASSENGER</h1>
+);
+};
+
+export default PassengerInfo;
